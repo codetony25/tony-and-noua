24d1b8156ed64f9d841fc67b2069c27aabe227b6
codetony25 <me@codetony.com>
2018-08-04 23:45:48 -0700

    change ports

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const port = process.env.PORT || 2000;
+export const port = process.env.PORT || 3030;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
 export const databaseUrl = process.env.DATABASE_URL || 'sqlite:database.sqlite';
